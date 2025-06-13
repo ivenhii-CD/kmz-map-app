af888489860834ddfe80dc14def806e4d224fb98
Iven Hii <ivenofficeuse@gmail.com>
2025-06-13 15:43:04 +0800

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,14 +20,15 @@ export class AppComponent implements OnInit {
   initMap(): void {
     this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
       center: { lat: 48.8584, lng: 2.2945 },
-      zoom: 15
+      zoom: 16
     });
 
+
     this.kmzLayer = new google.maps.KmlLayer({
-      url: 'https://ivenhii-cd.github.io/kmz-map-app/sample_eiffel_marker.kml',
-      map: this.map,
-      preserveViewport: true,
-      suppressInfoWindows: false
-    });
+    url: 'https://ivenhii-cd.github.io/kmz-map-app/sample_eiffel_marker.kml',
+    map: this.map,
+    preserveViewport: true
+  });
+
   }
 }
