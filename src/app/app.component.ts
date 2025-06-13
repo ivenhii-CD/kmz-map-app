adf182a5eaf2c8f764ee27fff449a738d0671fc4
Iven Hii <ivenofficeuse@gmail.com>
2025-06-13 15:46:03 +0800

    upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,16 +19,15 @@ export class AppComponent implements OnInit {
 
   initMap(): void {
     this.map = new google.maps.Map(document.getElementById('map') as HTMLElement, {
-      center: { lat: 48.8584, lng: 2.2945 },
-      zoom: 16
+      center: { lat: 48.8584, lng: 2.2945 }, // Eiffel Tower
+      zoom: 15
     });
 
-
     this.kmzLayer = new google.maps.KmlLayer({
-    url: 'https://ivenhii-cd.github.io/kmz-map-app/sample_eiffel_marker.kml',
-    map: this.map,
-    preserveViewport: true
-  });
-
+      url: 'https://ivenhii-cd.github.io/kmz-map-app/sample_eiffel_marker.kml',
+      map: this.map,
+      preserveViewport: true,
+      suppressInfoWindows: false
+    });
   }
 }
